251a9ad7107b434e3df7500129a45ed8b6adad19
XNCOAISN <end.pgm@gmail.com>
2023-07-29 23:20:26 +0900

    🔧 jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(config);

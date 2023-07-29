9f0217dee2fbbd2c17bd6b0255d0c1705ba2b42f
XNCOAISN <end.pgm@gmail.com>
2023-07-30 04:10:46 +0900

    ♻️ type define

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,10 +1,10 @@
 import { clsx } from "clsx";
-import Link, { LinkProps } from "next/link";
+import Link from "next/link";
 import { ComponentPropsWithoutRef } from "react";
 
 import * as styles from "@/components/Button/Button.css";
 
-export type LinkButtonProps = ComponentPropsWithoutRef<"a"> & LinkProps;
+export type LinkButtonProps = ComponentPropsWithoutRef<typeof Link>;
 
 export const LinkButton = ({ className, ...props }: LinkButtonProps) => {
   return <Link {...props} className={clsx(styles.button, className)} />;
